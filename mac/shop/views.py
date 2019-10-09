0eebc7ace74e155ee3485bf6a5df0b1a3b24331f
lordkratos <shaurabhmishra2014@gmail.com>
2019-10-09 23:49:33 +0530

    Minor fixes.

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 
 def index(request):
-    return render(request, "shop/index.html")
+    products = Product.objects.all()
+    print(products)
+    nSlides = (n // 4) + ceil((n / 4) - (n // 4))
+    params = {'no_of_slides': nSlides, 'range': range(1,nSlides), 'product': products}
+    return render(request, "shop/index.html", params)
+
 
 def about(request):
-	return HttpResponse("We are at about")
+    return render(request, "shop/about.html")
+
 
 def contact(request):
-	return HttpResponse("We are at contact")
+    return HttpResponse("We are at contact")
+
 
 def tracker(request):
-	return HttpResponse("We are at tracker")
+    return HttpResponse("We are at tracker")
+
 
 def search(request):
-	return HttpResponse("We are at search")
+    return HttpResponse("We are at search")
+
 
 def productView(request):
-	return HttpResponse("We are at productView")
+    return HttpResponse("We are at productView")
+
 
 def checkout(request):
-	return HttpResponse("We are at checkout")
+    return HttpResponse("We are at checkout")
